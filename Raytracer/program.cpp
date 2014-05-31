711b82faaef0dd86657222cbed47584c8936b6e1
Sebastian Larsson <sjereq@gmail.com>
2014-05-31 21:54:55 +0200

    Remove unused context printing functions

--- Raytracer/program.cpp
+++ Raytracer/program.cpp
@@ -49,74 +49,9 @@ void startTests()
 	logFile.close();
 }
 
-void printPlatformInfo(cl::Platform& plat)
-{
-	std::string profile = plat.getInfo<CL_PLATFORM_PROFILE>();
-	std::string version = plat.getInfo<CL_PLATFORM_VERSION>();
-	std::string name = plat.getInfo<CL_PLATFORM_NAME>();
-	std::string vendor = plat.getInfo<CL_PLATFORM_VENDOR>();
-	std::string extensions = plat.getInfo<CL_PLATFORM_EXTENSIONS>();
-
-	std::cout << "  Profile    : " << profile << std::endl;
-	std::cout << "  Version    : " << version << std::endl;
-	std::cout << "  Name       : " << name << std::endl;
-	std::cout << "  Vendor     : " << vendor << std::endl;
-	std::cout << "  Extensions : " << extensions << std::endl;
-}
-
-void printContextInfo(cl::Context& con)
-{
-	std::cout << "  Reference count : " << con.getInfo<CL_CONTEXT_REFERENCE_COUNT>() << std::endl;
-	std::cout << "  Num devices     : " << con.getInfo<CL_CONTEXT_NUM_DEVICES>() << std::endl;
-}
-
-void printDeviceInfo(cl::Device& dev)
-{
-	//std::cout << "  Address bits              : " << dev.getInfo<CL_DEVICE_ADDRESS_BITS>() << std::endl;
-	std::cout << "  Available                 : " << dev.getInfo<CL_DEVICE_AVAILABLE>() << std::endl;
-	//std::cout << "  Compiler available        : " << dev.getInfo<CL_DEVICE_COMPILER_AVAILABLE>() << std::endl;
-	//std::cout << "  Double fp config          : " << dev.getInfo<CL_DEVICE_DOUBLE_FP_CONFIG>() << std::endl;
-	//std::cout << "  Endian little             : " << dev.getInfo<CL_DEVICE_ENDIAN_LITTLE>() << std::endl;
-	//std::cout << "  Error correction support  : " << dev.getInfo<CL_DEVICE_ERROR_CORRECTION_SUPPORT>() << std::endl;
-	//std::cout << "  Execution capabilities    : " << dev.getInfo<CL_DEVICE_EXECUTION_CAPABILITIES>() << std::endl;
-	//std::cout << "  Extensions                : " << dev.getInfo<CL_DEVICE_EXTENSIONS>() << std::endl;
-	std::cout << "  Global mem cache size     : " << dev.getInfo<CL_DEVICE_GLOBAL_MEM_CACHE_SIZE>() << std::endl;
-	//std::cout << "  Global mem cache type     : " << dev.getInfo<CL_DEVICE_GLOBAL_MEM_CACHE_TYPE>() << std::endl;
-	std::cout << "  Global mem cacheline size : " << dev.getInfo<CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE>() << std::endl;
-	std::cout << "  Global mem size           : " << dev.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>() << std::endl;
-	std::cout << "  Host unified memory       : " << dev.getInfo<CL_DEVICE_HOST_UNIFIED_MEMORY>() << std::endl;
-	//std::cout << "  Image support             : " << dev.getInfo<CL_DEVICE_IMAGE_SUPPORT>() << std::endl;
-	//std::cout << "  Image 2D max height       : " << dev.getInfo<CL_DEVICE_IMAGE2D_MAX_HEIGHT>() << std::endl;
-	//std::cout << "  Image 2D max width        : " << dev.getInfo<CL_DEVICE_IMAGE2D_MAX_WIDTH>() << std::endl;
-	std::cout << "  Local mem size            : " << dev.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() << std::endl;
-	std::cout << "  Local mem type            : " << dev.getInfo<CL_DEVICE_LOCAL_MEM_TYPE>() << std::endl;
-	std::cout << "  Max clock frequency       : " << dev.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << std::endl;
-	std::cout << "  Max compute units         : " << dev.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl;
-	std::cout << "  Max constant args         : " << dev.getInfo<CL_DEVICE_MAX_CONSTANT_ARGS>() << std::endl;
-	std::cout << "  Max constant buffer size  : " << dev.getInfo<CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE>() << std::endl;
-	std::cout << "  Max mem alloc size        : " << dev.getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>() << std::endl;
-	std::cout << "  Max parameter size        : " << dev.getInfo<CL_DEVICE_MAX_PARAMETER_SIZE>() << std::endl;
-	std::cout << "  Max read image args       : " << dev.getInfo<CL_DEVICE_MAX_READ_IMAGE_ARGS>() << std::endl;
-	std::cout << "  Max samplers              : " << dev.getInfo<CL_DEVICE_MAX_SAMPLERS>() << std::endl;
-	std::cout << "  Max work group size       : " << dev.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << std::endl;
-	std::cout << "  Max work item dimensions  : " << dev.getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>() << std::endl;
-
-	std::vector<size_t> maxItemSizes = dev.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
-	for (size_t i = 0; i < maxItemSizes.size(); i++)
-	{
-		std::cout << "  Max work item sizes[" << i << "]    : " << maxItemSizes[i] << std::endl;
-	}
-	std::cout << "  Max write image args      : " << dev.getInfo<CL_DEVICE_MAX_WRITE_IMAGE_ARGS>() << std::endl;
-	std::cout << "  Name                      : " << dev.getInfo<CL_DEVICE_NAME>() << std::endl;
-	std::cout << "  Version                   : " << dev.getInfo<CL_DEVICE_VERSION>() << std::endl;
-	std::cout << "  Profiling timer resolution: " << dev.getInfo<CL_DEVICE_PROFILING_TIMER_RESOLUTION>() << std::endl;
-}
-
-void initCL(cl::Context& _context, std::vector<cl::Device>& _devices, cl::CommandQueue& _queue, bool printInfo)
+void initCL(cl::Context& _context, std::vector<cl::Device>& _devices, cl::CommandQueue& _queue)
 {
 	cl_int err = CL_SUCCESS;
-
-	//std::cout << "Getting platforms..." << std::endl;
 
 	std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
@@ -124,20 +59,6 @@ void initCL(cl::Context& _context, std::vector<cl::Device>& _devices, cl::Comman
 	{
 		throw std::exception("No OpenCL platform found.");
 	}
-
-	unsigned int c = 0;
-	if (printInfo)
-	{
-		for (auto p : platforms)
-		{
-			std::cout << "Platform " << c++ << ":" << std::endl;
-			printPlatformInfo(p);
-		}
-
-		std::cout << "Using platform 0" << std::endl;
-	}
-
-	//std::cout << "Creating context..." << std::endl;
 
 	HGLRC glCtx = wglGetCurrentContext();
 
@@ -162,23 +83,9 @@ void initCL(cl::Context& _context, std::vector<cl::Device>& _devices, cl::Comman
 	cl_int status = clGetGLContextInfoKHR(properties, CL_CURRENT_DEVICE_FOR_GL_CONTEXT_KHR, sizeof(cl_device_id), &interopDevice, nullptr);
 	cl::Device dev(interopDevice);
 
-	if (printInfo)
-	{
-		std::cout << "Device:" << std::endl;
-		printDeviceInfo(dev);
-	}
-
 	_devices.push_back(dev);
 
 	_context = cl::Context(_devices, properties);
-
-	if (printInfo)
-	{
-		std::cout << "Context:" << std::endl;
-		printContextInfo(_context);
-	}
-
-	//std::cout << "Creating queue..." << std::endl;
 
 	_queue = cl::CommandQueue(_context, dev, CL_QUEUE_PROFILING_ENABLE, &err);
 }
@@ -836,7 +743,7 @@ int main(int argc, char** argv)
 		cl::Context context;
 		std::vector<cl::Device> devices;
 		cl::CommandQueue queue;
-		initCL(context, devices, queue, false);
+		initCL(context, devices, queue);
 
 		window.createFramebuffer(windowWidth, windowHeight);
 		
